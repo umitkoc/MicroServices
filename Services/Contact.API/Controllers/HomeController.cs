5c16b862249d18a90d9a8ee4b9a4d97726d5d14c
umitkoc <umitkoc.com@gmail.com>
2024-07-27 01:26:48 +0300

    init commit

---
+++ Services/Contact.API/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class HomeController : ControllerBase
+    {
+      
+    }
+}
