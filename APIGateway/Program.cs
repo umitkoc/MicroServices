ccca4c6b40e7a9adbcb13ac609be01323c1ef2a9
umitkoc <umitkoc.com@gmail.com>
2024-07-30 00:56:49 +0300

    update ocelot file in the  gateway service

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -5,31 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.
     AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).
     AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true).
-    AddJsonFile("ocelot.json");
-builder.Services.AddOcelot();
-// Add services to the container.
-
+    AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
 builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
-app.UseOcelot().Wait();
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
-//app.MapControllers();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
-
+await app.UseOcelot();
 app.Run();
